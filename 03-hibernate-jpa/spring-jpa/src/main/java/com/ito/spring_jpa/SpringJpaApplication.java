5b7cc2ba039087ca8424f478b7902f1d37ae20c2
datnt170 <ito.ruan196@gmail.com>
2025-04-08 20:06:54 +0700

    "commit"

---
+++ 03-hibernate-jpa/spring-jpa/src/main/java/com/ito/spring_jpa/SpringJpaApplication.java
@@ -0,0 +1,22 @@
+package com.ito.spring_jpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
