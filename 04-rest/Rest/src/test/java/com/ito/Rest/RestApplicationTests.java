1f79e18908e2013a5498f728b1ebd3317c9b4fdd
datnt170 <ito.ruan196@gmail.com>
2025-04-09 20:17:28 +0700

    04-rest

---
+++ 04-rest/Rest/src/test/java/com/ito/Rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ito.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
