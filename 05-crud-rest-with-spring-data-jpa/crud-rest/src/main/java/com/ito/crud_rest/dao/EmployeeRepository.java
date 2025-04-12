4bc1b5ac17e4338f4b457e8498e145ed03e12754
datnt170 <ito.ruan196@gmail.com>
2025-04-12 07:27:24 +0700

    07-crud-rest-spring-data-jpa-rest

--- 05-crud-rest-with-spring-data-jpa/crud-rest/src/main/java/com/ito/crud_rest/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ito.crud_rest.dao;
-
-import com.ito.crud_rest.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // No additional methods are needed here as JpaRepository provides all the necessary CRUD operations
-
-}
