fb482be63c75282b80d3fdfb9f3b32ce1209a0d8
datnt170 <ito.ruan196@gmail.com>
2025-04-16 20:44:43 +0700

    final 11

--- 11-spring-mvc-security/springmvcsecurity/src/main/java/com/ito/springmvcsecurity/security/DemoSecurityConfig.java
+++ 11-spring-mvc-security/springmvcsecurity/src/main/java/com/ito/springmvcsecurity/security/DemoSecurityConfig.java
@@ -35,13 +35,17 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(form ->
                         form
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
-                );
+                ).logout(logout -> logout.permitAll()).exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
         return http.build();
     }
 }
